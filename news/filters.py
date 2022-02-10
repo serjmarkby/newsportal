f2ce97c190ad38f833891458f0a1b635a9b9e2b5
SerjMarkby <serj.markby@gmail.com>
2022-02-10 21:15:31 +0300

    Add filters from Post

---
+++ news/filters.py
@@ -0,0 +1,12 @@
+from django_filters import FilterSet  # импортируем filterset, чем-то напоминающий знакомые дженерики
+from .models import Post
+
+
+# создаём фильтр
+class PostFilter(FilterSet):
+    # Здесь в мета классе надо предоставить модель и указать поля, по которым будет фильтроваться
+    # (т. е. подбираться) информация о товарах
+    class Meta:
+        model = Post
+        fields = ('author', 'categoryType', 'postCategory', 'rating', 'title', 'text')
+        # поля, которые мы будем фильтровать(т. е. отбирать по каким-то критериям, имена берутся из моделей)
