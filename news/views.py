98c798f141236ae8323c0ec4e3641deda18020cc
SerjMarkby <serj.markby@gmail.com>
2022-02-10 21:16:16 +0300

    Добавлен метод get_context_data

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DeleteView
+from django.core.paginator import Paginator
+
 from .models import Author, Post, PostCategory, Comment, Category
+from .filters import PostFilter
 
 # Create your views here.
 
@@ -11,6 +14,11 @@ class NewsList(ListView):
     context_object_name = 'news'
     queryset = Post.objects.order_by('-dateCreation')
     paginate_by = 9
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        return context
 
 
 class NewsDetail(DeleteView):
