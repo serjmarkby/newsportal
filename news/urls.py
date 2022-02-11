cc545266de63ae731429f8f59dc4016e0f8460a4
SerjMarkby <serj.markby@gmail.com>
2022-02-11 19:40:04 +0300

    Добавлены новые URLS

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import NewsList, NewsDetail, NewsCreate
+from .views import NewsList, NewsDetail, NewsCreate, SearchList, NewsUpdate, NewsDelete
 
 
 urlpatterns = [
     path('', NewsList.as_view()),
     path('<int:pk>', NewsDetail.as_view(), name='post'),
     path('create/', NewsCreate.as_view(), name='news_create'),
+    path('search/', SearchList.as_view(), name='search'),
+    path('update/', NewsUpdate.as_view(), name='news_create'),
+    path('delete/', NewsDelete.as_view(), name='news_delete'),
 ]
