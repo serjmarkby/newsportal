5de5b79cb7b01846d559ff6017bb7a9e6551464d
SerjMarkby <serj.markby@gmail.com>
2022-02-11 18:22:15 +0300

    Добавлен чекбокс для подтверждения намерений создать новость

--- news/forms.py
+++ news/forms.py
@@ -1,9 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 from .models import Post
 
 
 # Создаём модельную форму
 class NewsForm(ModelForm):
+    check_box = BooleanField(label='Даю себе отчет')  # добавляем галочку, или же true-false поле
+
     # в класс мета, как обычно, надо написать модель, по которой будет строится форма
     # и нужные нам поля. Мы уже делали что-то похожее с фильтрами.
     class Meta:
