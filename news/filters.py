6ef44117fdf643398720c99864e4be042900202c
SerjMarkby <serj.markby@gmail.com>
2022-02-11 18:23:09 +0300

    Переопределил параметр dateCreation для вывода поля Даты более красиво

--- news/filters.py
+++ news/filters.py
@@ -1,18 +1,28 @@
-from django_filters import FilterSet  # импортируем filterset, чем-то напоминающий знакомые дженерики
+from django_filters import FilterSet, DateFilter  # импортируем filterset, чем-то напоминающий знакомые дженерики
 from .models import Post
+import django.forms
 
 
 # создаём фильтр
 class NewsFilter(FilterSet):
+    dateCreation = DateFilter(
+        lookup_expr='gte',
+        widget=django.forms.DateInput(
+            attrs={
+                'type': 'date'
+            }
+        )
+    )
+
     # Здесь в мета классе надо предоставить модель и указать поля, по которым будет фильтроваться
     # (т. е. подбираться) информация о товарах
+
     class Meta:
         model = Post
-        # fields = ('author', 'dateCreation', 'postCategory', 'rating')
+
         # поля, которые мы будем фильтровать(т. е. отбирать по каким-то критериям, имена берутся из моделей)
         fields = {
             'author': ['exact'],
-            'dateCreation': ['gt'],
             'postCategory': ['exact'],
             'rating': ['gt'],
         }
