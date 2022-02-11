f5dccb8dc1e4d2d34a69c11f8f72fb70cc706ea9
SerjMarkby <serj.markby@gmail.com>
2022-02-11 19:13:39 +0300

    Добавлен метод для переадрессации на страницу новости после её создания

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewsList, NewsDetail
+from .views import NewsList, NewsDetail, NewsCreate
 
 
 urlpatterns = [
     path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view())
+    path('<int:pk>', NewsDetail.as_view(), name='post'),
+    path('create/', NewsCreate.as_view(), name='news_create'),
 ]
