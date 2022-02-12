896e35722d9d57251ef2b4c55b4d23e6293fbcdb
SerjMarkby <serj.markby@gmail.com>
2022-02-12 12:14:13 +0300

    Добавил форму для редактирования User

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, BooleanField
 from .models import Post
+from django.contrib.auth.models import User
 
 
 # Создаём модельную форму
@@ -11,3 +12,14 @@ class NewsForm(ModelForm):
     class Meta:
         model = Post
         fields = ['author', 'title', 'text', 'postCategory', 'postPhoto']
+
+
+# Создаём модельную форму
+class UserForm(ModelForm):
+    check_box = BooleanField(label='Даю себе отчет')  # добавляем галочку, или же true-false поле
+
+    # в класс мета, как обычно, надо написать модель, по которой будет строится форма
+    # и нужные нам поля. Мы уже делали что-то похожее с фильтрами.
+    class Meta:
+        model = User
+        fields = ['username', 'last_name', 'first_name', 'email']
