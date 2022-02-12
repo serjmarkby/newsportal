ee0f253de8bc47dd97e222be58bd1b9ec5222410
SerjMarkby <serj.markby@gmail.com>
2022-02-12 21:10:00 +0300

    Полностью пересобрал все url, чтобы можно было и редактировать автора

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import NewsList, NewsDetail, NewsCreate, SearchList, NewsUpdate, NewsDelete
+from .views import NewsList, NewsDetail, NewsCreate, SearchList, NewsUpdate, NewsDelete, UserUpdate, AuthorDetail
 
 
 urlpatterns = [
-    path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view(), name='post'),
-    path('create/', NewsCreate.as_view(), name='news_create'),
+    path('news/', NewsList.as_view(),name='news'),
+    path('news/<int:pk>', NewsDetail.as_view(), name='post'),
+    path('news/create/', NewsCreate.as_view(), name='news_create'),
     path('search/', SearchList.as_view(), name='search'),
-    path('<int:pk>/update/', NewsUpdate.as_view(), name='news_create'),
-    path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('news/<int:pk>/update/', NewsUpdate.as_view(), name='news_create'),
+    path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+    path('author/<int:pk>/', AuthorDetail.as_view(), name='author'),
+    path('author/<int:pk>/edit/', UserUpdate.as_view(), name='edit_user'),
 ]
